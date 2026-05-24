a1f5d7a8e963c5578d8b2ac11adc7e934d8d3f3e
mr-devloper005 <mr.opxoo5@gmail.com>
2026-05-24 23:31:25 +0530

    Initialize Slot 4 site for xperthinks.com [skip ci]

---
+++ src/config/site.factory.ts
@@ -0,0 +1,14 @@
+import type { SiteFactoryRecipe } from '@/design/factory/types'
+
+export const SITE_FACTORY_RECIPE: SiteFactoryRecipe = {
+  brandPack: 'editorial-luxe',
+  navbar: 'compact-bar',
+  footer: 'editorial-footer',
+  homeLayout: 'article-home',
+  motionPack: 'editorial-soft',
+  primaryTask: 'article',
+  enabledTasks: ["article"],
+  taskLayouts: {
+    article: 'article-editorial',
+  },
+}
